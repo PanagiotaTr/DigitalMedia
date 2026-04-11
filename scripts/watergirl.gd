652da6c1e6486ddfa394be728ce38e0af1646271
Panagiota.tr <panayiota03@gmail.com>
2026-04-11 18:56:36 +0300

    create watergirl character

---
+++ scripts/watergirl.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+const SPEED = 230.0
+const JUMP_VELOCITY = -300.0
+
+@onready var body_sprite = $Body
+@onready var head_sprite = $Head
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("watergirl_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("watergirl_left", "watergirl_right")
+
+	if direction < 0:
+		body_sprite.flip_h = true
+		head_sprite.flip_h = true
+	elif direction > 0:
+		body_sprite.flip_h = false
+		head_sprite.flip_h = false
+
+	if is_on_floor():
+		if direction == 0:
+			body_sprite.play("idle")
+			head_sprite.play("idle")
+		else:
+			body_sprite.play("run")
+			head_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			body_sprite.play("idle")
+			head_sprite.play("jump")
+		else:
+			body_sprite.play("idle")
+			head_sprite.play("fall")
+
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
