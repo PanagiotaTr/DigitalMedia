f54a8e5a7aea66b284173a0c68af6f1da9fa7b0a
Panagiota.tr <panayiota03@gmail.com>
2026-04-15 14:40:59 +0300

    4/4 coins ready

--- scripts/red_coin.gd
+++ scripts/red_coin.gd
@@ -1,8 +1,5 @@
 extends Area2D
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
-		
-	#print("Mphke")
-	#if body.is_in_group("fireboy"):
-		#queue_free()
-	queue_free()
+	animation_player.play("pickup")
